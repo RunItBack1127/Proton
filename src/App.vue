47129b6044580b8176716595d6626efd2740bb6f
RunItBack1127 <thewikiscience@gmail.com>
2023-02-05 14:23:22 -0500

    Outlining initial viewer markup

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
+  <div id="ProtonModelViewer"></div>
+  <nav class="OptionsPanel">
+    
+  </nav>
+  <aside class="ViewerPropertiesPanel">
 
+  </aside>
+  <div class="ObjectInfoWidget">
+    
+  </div>
 </template>
 
 <style scoped lang="scss">
