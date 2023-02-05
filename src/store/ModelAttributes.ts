9221321977a21fc1c761ba3fb45e0c5b35258e18
RunItBack1127 <thewikiscience@gmail.com>
2023-02-05 15:32:45 -0500

    Removing verbose setters and getters

--- src/store/ModelAttributes.ts
+++ src/store/ModelAttributes.ts
@@ -8,34 +8,6 @@ const useModelAttributesStore = defineStore('attributes', {
             numTriangles: 0,
             loadDuration: 0
         }
-    },
-    actions: {
-        setAnimationId(animationId: number) {
-            this.currentAnimationId = animationId;
-        },
-        setNumVertices(numVertices: number) {
-            this.numVertices = numVertices;
-        },
-        setNumTriangles(numTriangles: number) {
-            this.numTriangles = numTriangles;
-        },
-        setLoadDuration(loadDuration: number) {
-            this.loadDuration = loadDuration;
-        }
-    },
-    getters: {
-        getAnimationId(): number {
-            return this.currentAnimationId
-        },
-        getNumVertices(): number {
-            return this.numVertices
-        },
-        getNumTriangles(): number {
-            return this.numTriangles
-        },
-        getLoadDuration(): number {
-            return this.loadDuration
-        }
     }
 });
 
