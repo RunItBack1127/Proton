ab1be1dd1a1561f4f51e7c57a1c7fd856ca67952
RunItBack1127 <thewikiscience@gmail.com>
2023-02-05 14:26:13 -0500

    Implementing initial component setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
+import ObjectInfoWidget from './components/ObjectInfoWidget.vue';
+import OptionsPanel from './components/OptionsPanel.vue';
+import ViewerPropertiesPanel from './components/ViewerPropertiesPanel.vue';
 
 </script>
 
 <template>
   <div id="ProtonModelViewer"></div>
   <nav class="OptionsPanel">
-    
+    <options-panel />
   </nav>
   <aside class="ViewerPropertiesPanel">
-
+    <viewer-properties-panel />
   </aside>
   <div class="ObjectInfoWidget">
-    
+    <object-info-widget />
   </div>
 </template>
 
